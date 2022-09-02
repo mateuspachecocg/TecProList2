d4d7fb181df51729c12983b6393c294f9151f04d
Mateus Pacheco <mateuspacheco@alu.ufc.br>
2022-09-02 14:42:42 -0300

    Adding the project on GitHub

---
+++ src/solutions/Main.java
@@ -0,0 +1,14 @@
+package solutions;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello EveryBody");
+		
+		System.out.println("Hello EveryBody2");
+
+		System.out.println("Hello EveryBody3");
+	}
+
+}
