bf294622e3639ef18ca5829c31272569b376443c
Mateus Pacheco <mateuspacheco@alu.ufc.br>
2022-09-03 20:35:05 -0300

    Adding Question12

--- src/solutions/Question11.java
+++ src/solutions/Question11.java
@@ -10,10 +10,8 @@ public class Question11 {
 	public static void solution(int ll, int lr) {
 		int i = 0;
 		int j = 0;
-		
-		
-		for (;i  < ll+2; i++ ) {
 			
+		for (;i  < ll+2; i++ ) {
 			for (j = 1;j <= lr; j++) {
 				int k = 0;
 				for (;k <= j; k++) {
