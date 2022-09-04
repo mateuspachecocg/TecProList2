2e029905ddee32c0627802ee6dd34018be0ede69
Mateus Pacheco <mateuspacheco@alu.ufc.br>
2022-09-04 20:02:22 -0300

    Finish item A

--- src/solutions/Question12.java
+++ src/solutions/Question12.java
@@ -4,7 +4,23 @@ public class Question12 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		// Test for each item
+		// itemA(6, 6);
+	}
+	
+	public static void itemA(int row, int column) {
+		int i = 1;
+		int j = 1;
+		
+		for (;i <= row; i++) {
+			for ( ;j  <= column; j++) {
+				if (i >= j) {
+					System.out.print(" * ");
+				}
+			}
+			System.out.println();
+			j = 1;
+		}
 	}
 
 }
