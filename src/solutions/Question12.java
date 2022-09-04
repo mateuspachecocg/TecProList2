702ff4b0d9b4705a301a7fe0f155cb499c0cdb64
Mateus Pacheco <mateuspacheco@alu.ufc.br>
2022-09-04 20:22:23 -0300

    Finish Question12

--- src/solutions/Question12.java
+++ src/solutions/Question12.java
@@ -6,9 +6,14 @@ public class Question12 {
 		// TODO Auto-generated method stub
 		// Test for each item
 		// itemA(6, 6);
+		// itemB(6,6);
+		// itemC(6,6);
+		// itemD(6,6); 
+		itemE(6,6);
+		
 	}
 	
-	public static void itemA(int row, int column) {
+	public static void itemA (int row, int column) {
 		int i = 1;
 		int j = 1;
 		
@@ -22,5 +27,82 @@ public class Question12 {
 			j = 1;
 		}
 	}
-
+	
+	public static void itemB (int row, int column) {
+		int i = 1;
+		int j = 1;
+		
+		for (;i <= row; i++) {
+			
+			for ( ;j  <= column; j++) {
+				if ((i == j) || (i + j == (column+row)/2 + 1)) {
+					System.out.print(" # ");
+				} else {
+					System.out.print(" * ");
+				}
+			}
+			
+			System.out.println();
+			j = 1;
+		}
+	}
+	
+	public static void itemC (int row, int column) {
+		int i = 1;
+		int j = 1;
+		
+		for (;i <= row; i++) {
+			
+			for ( ;j  <= column; j++) {
+				if (i%2 != 0) {
+					System.out.print(" # ");
+				} else {
+					System.out.print(" * ");
+				}
+			}
+			
+			System.out.println();
+			j = 1;
+		}
+	}
+	
+	public static void itemD (int row, int column) {
+		int i = 1;
+		int j = 1;
+		
+		for (;i <= row; i++) {
+			
+			for ( ;j  <= column; j++) {
+				if (i == 1 || j == 1 || i == row || j == column ) {
+					System.out.print(" # ");
+				} else {
+					System.out.print(" * ");
+				}
+			}
+			
+			System.out.println();
+			j = 1;
+		}
+	}
+	
+	public static void itemE (int row, int column) {
+		int i = 1;
+		int j = 1;
+		
+		for (;i <= row; i++) {
+			
+			for ( ;j  <= column; j++) {
+				if (i <= row/2) {
+					System.out.print(" * ");
+				} else if (j%2 != 0) {
+					System.out.print(" # ");
+				} else {
+					System.out.print(" $ ");
+				}
+			}
+			
+			System.out.println();
+			j = 1;
+		}
+	}
 }
